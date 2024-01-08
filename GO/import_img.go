9d936ab0b4faae9fed9d9f4f1072283fc25ab3ae
eoliennne <likavf3@gmail.com>
2024-01-08 15:09:52 +0100

    closes Func bord #3

--- GO/import_img.go
+++ GO/import_img.go
@@ -22,6 +22,39 @@ func decodeImage(filename string) (image.Image, string, error) {
 	return img, format, nil
 }
 
+// fonction qui permet de savoir à quel voisin d'un pixel on
+// applique le flou selon le rayon souhaité
+func Bord(x, y, r int, img image.Image) (xmin, xmax, ymin, ymax int) {
+
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+
+	if a := x - r; a < 0 {
+		xmin = 0
+	} else {
+		xmin = a
+	}
+
+	if a := x + r; a > width {
+		xmax = width
+	} else {
+		xmax = a
+	}
+
+	if a := y - r; a < 0 {
+		ymin = 0
+	} else {
+		ymin = a
+	}
+
+	if a := y + r; a > height {
+		ymax = height
+	} else {
+		ymax = a
+	}
+	return
+}
+
 func main() {
 	img, format, err := decodeImage("cat.png")
 	if err != nil {
