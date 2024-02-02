f6f952f0d81968a076e2b3d774004bc593f6937f
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-02-02 11:00:03 +0100

    JS : déplacement des fonctions dans joueur.js

--- JS/joueur.js
+++ JS/joueur.js
@@ -183,7 +183,84 @@ class joueur {
            
 };
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function initSac(){
+    let sac = []
+
+    lettres = [{l:"A",n:14},{l:"B",n:4},{l:"C",n:7},{l:"D",n:5}
+        ,{l:"E",n:19},{l:"F",n:2},{l:"G",n:4},{l:"H",n:2},{l:"I",n:11}
+        ,{l:"J",n:1},{l:"K",n:1},{l:"L",n:6},{l:"M",n:5},{l:"N",n:9}
+        ,{l:"O",n:8},{l:"P",n:4},{l:"Q",n:1},{l:"R",n:10},{l:"S",n:7}
+        ,{l:"T",n:9},{l:"U",n:8},{l:"V",n:2},{l:"W",n:1},{l:"X",n:1}
+        ,{l:"Y",n:1},{l:"Z",n:2}];
+
+    for (let i=0;i<lettres.length;i++){
+        let{l,n} = lettres[i];
+        for (j=0;j<n;j++){
+            sac.push(l);
+        }
+    }
+    shuffleArray(sac);
+    return sac;
+
+};
+
+function jouer(joueur,tour,jarnak,joueurB,sac){
+    // joueur est le joueur dont le deck et les mots sont utilisés
+    // joueurB est le joueur qui joue et place les mots sur sa grille
+    joueur.afficheGrille();
+    joueur.afficheDeck();
+    let status = joueur.nouveauMot(joueur.choixLigne(),jarnak,sac,joueurB);
+    if (status=="fini")
+    {
+        tour = (tour+1)%2 ;
+    }else if (status=="ok"){
+        joueur.jeu.push(sac.pop()); //pioche 1 lettre
+    }
+    return tour
+}
+
+function tourEntier(joueur,adversaire,sac){
+    console.log("\n-------------\nTOUR DU JOUEUR",joueur.num);
+    joueur.afficheGrille();
+    joueur.afficheDeck();
+    adversaire.afficheGrille();
+    adversaire.afficheDeck();
+
+    joueur.choixPioche(sac);
+    
+    console.log("Jarnak ? o/n");
+    const jarnak = tapeMot();
+    if (jarnak == "o"){
+        fs.appendFileSync("jeu.log","  Jarnak!\n",'utf-8')
+
+        // Le tour se déroule comme si c'était celui de joueur1 
+        // mais les mots sont ajoutés sur la grille du joueur2
+        tour = adversaire.num;
+        while (tour == adversaire.num){
+            tour = jouer(adversaire,tour,true,joueur,sac);
+       }
+    }
+
+    tour = joueur.num;
+    while (tour==joueur.num){
+        tour = jouer(joueur,tour,false,joueur,sac);    
+    }
+    
+}
+
+
+
 module.exports= {
     joueur : joueur,
-    tapeMot : tapeMot
+    tapeMot : tapeMot,
+    initSac : initSac,
+    jouer : jouer,
+    tourEntier : tourEntier,
 };
--- JS/main.js
+++ JS/main.js
@@ -4,37 +4,11 @@ const fs = require('fs');
 
 // Fonctions 
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-}
 
-function init_sac(){
-    let sac = []
-
-    lettres = [{l:"A",n:14},{l:"B",n:4},{l:"C",n:7},{l:"D",n:5}
-        ,{l:"E",n:19},{l:"F",n:2},{l:"G",n:4},{l:"H",n:2},{l:"I",n:11}
-        ,{l:"J",n:1},{l:"K",n:1},{l:"L",n:6},{l:"M",n:5},{l:"N",n:9}
-        ,{l:"O",n:8},{l:"P",n:4},{l:"Q",n:1},{l:"R",n:10},{l:"S",n:7}
-        ,{l:"T",n:9},{l:"U",n:8},{l:"V",n:2},{l:"W",n:1},{l:"X",n:1}
-        ,{l:"Y",n:1},{l:"Z",n:2}];
-
-    for (let i=0;i<lettres.length;i++){
-        let{l,n} = lettres[i];
-        for (j=0;j<n;j++){
-            sac.push(l);
-        }
-    }
-    shuffleArray(sac);
-    return sac
-
-};
     
 
 // start 
-const sac = init_sac();
+const sac = moduleJoueur.init_sac();
 const logFile = fs.writeFileSync("jeu.log", "", 'utf-8');
 const joueur1 = new joueur(sac,1);
 const joueur2 = new joueur(sac,2);
@@ -43,16 +17,16 @@ const joueur2 = new joueur(sac,2);
 fs.appendFileSync("jeu.log","Tour du joueur 1\n",'utf-8')
 let tour = 1;
 while (tour==1){
-    tour = jouer(joueur1,tour,false,joueur1);
+    tour = moduleJoueur.jouer(joueur1,tour,false,joueur1,sac);
 };
 
 // alternance des tours
 jeufini = false;
 while(!(jeufini)){
     
-    fs.appendFileSync("jeu.log","Tour du joueur 2\n",'utf-8')
-    tourEntier(joueur2,joueur1);
-    tourEntier(joueur1,joueur2);
+    fs.appendFileSync("jeu.log","Tour du joueur 2\n",'utf-8');
+    moduleJoueur.tourEntier(joueur2,joueur1,sac);
+    moduleJoueur.tourEntier(joueur1,joueur2,sac);
 
     if (joueur1.grillePleine()) {
         jeufini = true;
@@ -63,50 +37,5 @@ while(!(jeufini)){
         console.log("Le joueur 2 a gagné");
     }
     
-    
-}
-
-
-function jouer(joueur,tour,jarnak,joueurB){
-    // joueur est le joueur dont le deck et les mots sont utilisés
-    // joueurB est le joueur qui joue et place les mots sur sa grille
-    joueur.afficheGrille();
-    joueur.afficheDeck();
-    let status = joueur.nouveauMot(joueur.choixLigne(),jarnak,sac,joueurB);
-    if (status=="fini")
-    {
-        tour = (tour+1)%2 ;
-    }else if (status=="ok"){
-        joueur.jeu.push(sac.pop()); //pioche 1 lettre
-    }
-    return tour
-}
-
-function tourEntier(joueur,adversaire){
-    console.log("\n-------------\nTOUR DU JOUEUR",joueur.num)
-    joueur.afficheGrille();
-    joueur.afficheDeck();
-    adversaire.afficheGrille();
-    adversaire.afficheDeck();
-
-    joueur.choixPioche(sac)
-    
-    console.log("Jarnak ? o/n")
-    const jarnak = moduleJoueur.tapeMot()
-    if (jarnak == "o"){
-        fs.appendFileSync("jeu.log","  Jarnak!\n",'utf-8')
-
-        // Le tour se déroule comme si c'était celui de joueur1 
-        // mais les mots sont ajoutés sur la grille du joueur2
-        tour = adversaire.num;
-        while (tour == adversaire.num){
-            tour = jouer(adversaire,tour,true,joueur);
-       }
-    }
-
-    tour = joueur.num;
-    while (tour==joueur.num){
-        tour = jouer(joueur,tour,false,joueur);    
-    }
     
 }
