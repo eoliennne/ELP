4e3c20e414fc48a687ee117a2b3f39cc8ba53e7b
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-02-02 11:08:09 +0100

    JS : 1er tour du joueur 2

--- JS/main.js
+++ JS/main.js
@@ -2,22 +2,35 @@ const moduleJoueur = require('./joueur.js');
 const joueur = moduleJoueur.joueur;
 const fs = require('fs');
 
-// Fonctions 
-
-
-    
-
 // start 
-const sac = moduleJoueur.init_sac();
+const sac = moduleJoueur.initSac();
 const logFile = fs.writeFileSync("jeu.log", "", 'utf-8');
 const joueur1 = new joueur(sac,1);
 const joueur2 = new joueur(sac,2);
 
-// 1er tour joueur 1 (sans jarnak)
+// 1er tours 
 fs.appendFileSync("jeu.log","Tour du joueur 1\n",'utf-8')
 let tour = 1;
 while (tour==1){
     tour = moduleJoueur.jouer(joueur1,tour,false,joueur1,sac);
+};
+
+fs.appendFileSync("jeu.log","Tour du joueur 2\n",'utf-8');
+console.log("Jarnak ? o/n");
+    const jarnak = tapeMot();
+    if (jarnak == "o"){
+        fs.appendFileSync("jeu.log","  Jarnak!\n",'utf-8');
+
+        // Le tour se déroule comme si c'était celui de joueur1 
+        // mais les mots sont ajoutés sur la grille du joueur2
+        tour = 1;
+        while (tour == 1){
+            tour = jouer(joueur1,tour,true,joueur2,sac);
+       }
+    }
+tour = 2;
+while (tour==2){
+    tour = moduleJoueur.jouer(joueur2,tour,false,joueur2,sac);
 };
 
 // alternance des tours
